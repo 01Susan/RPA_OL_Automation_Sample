7f5d510ea8df01fa634c2230a39cfaf50a221a14
Susan <dhitalsusan2@gmail.com>
2022-09-19 17:24:09 +0545

    sorted carrier_list

--- worksheet.py
+++ worksheet.py
@@ -6,7 +6,9 @@ def worksheet_writer(carrier_list):
     row, col = 0, 0
     new_workbook = xlsxwriter.Workbook("sheet.xlsx")
     new_sheet = new_workbook.add_worksheet()
-    for i in (random.sample(carrier_list, 30)):
+    carrier_list = random.sample(carrier_list, 30)
+    carrier_list = sorted(carrier_list, key=lambda x: x["carrier"])
+    for i in carrier_list:
         new_sheet.write(row, col, i["mbl"])
         col += 1
         new_sheet.write(row, col, i["container"])
